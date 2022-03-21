fed51388abb72bbf826cb69d78f9c6acba27a6d9
MT Scripts <89866234+Marttins011@users.noreply.github.com>
2022-03-21 15:52:58 +0000

    Add files via upload

---
+++ client/main.lua
@@ -0,0 +1,93 @@
+local headerShown = false
+local sendData = nil
+
+-- Functions
+
+local function openMenu(data)
+    if not data or not next(data) then return end
+    SetNuiFocus(true, true)
+    headerShown = false
+    sendData = data
+    SendNUIMessage({
+        action = 'OPEN_MENU',
+        data = table.clone(data)
+    })
+end
+
+local function closeMenu()
+    sendData = nil
+    headerShown = false
+    SetNuiFocus(false)
+    SendNUIMessage({
+        action = 'CLOSE_MENU'
+    })
+end
+
+local function showHeader(data)
+    if not data or not next(data) then return end
+    headerShown = true
+    sendData = data
+    SendNUIMessage({
+        action = 'SHOW_HEADER',
+        data = table.clone(data)
+    })
+end
+
+-- Events
+
+RegisterNetEvent('qb-menu:client:openMenu', function(data)
+    openMenu(data)
+end)
+
+RegisterNetEvent('qb-menu:client:closeMenu', function()
+    closeMenu()
+end)
+
+-- NUI Callbacks
+
+RegisterNUICallback('clickedButton', function(option)
+    if headerShown then headerShown = false end
+    PlaySoundFrontend(-1, 'Highlight_Cancel', 'DLC_HEIST_PLANNING_BOARD_SOUNDS', 1)
+    SetNuiFocus(false)
+    if sendData then
+        local data = sendData[tonumber(option)]
+        sendData = nil
+        if data then
+            if data.params.event then
+                if data.params.isServer then
+                    TriggerServerEvent(data.params.event, data.params.args)
+                elseif data.params.isCommand then
+                    ExecuteCommand(data.params.event)
+                elseif data.params.isQBCommand then
+                    TriggerServerEvent('QBCore:CallCommand', data.params.event, data.params.args)
+                elseif data.params.isAction then
+                    data.params.event(data.params.args)
+                else
+                    TriggerEvent(data.params.event, data.params.args)
+                end
+            end
+        end
+    end
+end)
+
+RegisterNUICallback('closeMenu', function()
+    headerShown = false
+    sendData = nil
+    SetNuiFocus(false)
+end)
+
+-- Command and Keymapping
+
+RegisterCommand('+playerfocus', function()
+    if headerShown then
+        SetNuiFocus(true, true)
+    end
+end)
+
+RegisterKeyMapping('+playerFocus', 'Give Menu Focus', 'keyboard', 'LMENU')
+
+-- Exports
+
+exports('openMenu', openMenu)
+exports('closeMenu', closeMenu)
+exports('showHeader', showHeader)
